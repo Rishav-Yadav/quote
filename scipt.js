3d296495012da7e009c205f54a3eae577331cceb
Rishav Yadav <rishavyadav154@gmail.com>
2024-10-08 15:30:18 +0530

    Final Version

--- scipt.js
+++ scipt.js
@@ -1,18 +1,39 @@
-const xhr = new XMLHttpRequest();
-xhr.open("GET", "https://animechan.io/api/v1/quotes/random");
-xhr.onreadystatechange = function () {
-  if (this.status === 200 && this.readyState === 4) {
-    const data = JSON.parse(this.responseText);
-    const quote = data[1].content;
-    const anime = data[1].anime.name;
-    const char = data[2].character.name;
-    const heading = document.createElement("h2");
-    heading.appendChild(document.createTextNode(quote));
-    const div = document.querySelector("div");
-    div.appendChild(heading);
-    const by = document.createTextNode(`~${char}(${anime})`);
-    by.style.textAlign = "right";
-    div.appendChild(by);
-  }
-};
-xhr.send();
+function getQuote() {
+  const xhr = new XMLHttpRequest();
+  xhr.open(
+    "GET",
+    "https://api.allorigins.win/get?url=" +
+      encodeURIComponent("https://animechan.io/api/v1/quotes/random")
+  );
+  xhr.onreadystatechange = function () {
+    if (this.status === 200 && this.readyState === 4) {
+      const contentsJSON = JSON.parse(this.responseText);
+      const dataObj = JSON.parse(contentsJSON.contents);
+      const quote = dataObj.data.content;
+      const anime = dataObj.data.anime.name;
+      const char = dataObj.data.character.name;
+      const heading = document.createElement("h2");
+      heading.appendChild(document.createTextNode(`"${quote}"`));
+      const div = document.querySelector("div");
+      div.innerHTML = "";
+      const strong = document.createElement("strong");
+      const by = document.createTextNode(`~${char}(${anime})`);
+      strong.appendChild(by);
+      strong.style.color = "yellow";
+      strong.style.textAlign = "right";
+
+      div.appendChild(heading);
+      div.appendChild(strong);
+      // console.log(
+      //   dataObj.data,
+      //   "lalalalal",
+      //   dataObj.data.content,
+      //   dataObj.data.anime.name,
+      //   dataObj.data.character.name
+      // );
+    }
+  };
+  xhr.send();
+}
+
+document.querySelector("button").addEventListener("click", getQuote);
